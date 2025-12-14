2d55b510a38ae3cf0d2527cd2586461b0d92a0ef
riveur <riveur.cpy@gmail.com>
2025-12-14 07:42:46 +0100

    fix(server): add missing env key

--- apps/server/start/env.ts
+++ apps/server/start/env.ts
@@ -33,6 +33,8 @@ export default await Env.create(new URL('../', import.meta.url), {
   DISCORD_CLIENT_ID: Env.schema.string(),
   DISCORD_CLIENT_SECRET: Env.schema.string(),
 
+  DISCORD_BOT_TOKEN: Env.schema.string(),
+
   REDIS_HOST: Env.schema.string({ format: 'host' }),
   REDIS_PORT: Env.schema.number(),
   REDIS_PASSWORD: Env.schema.string.optional(),
